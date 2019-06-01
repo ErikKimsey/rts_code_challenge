f2e7fde1b618560e77e2078db3dbf18e2e5e994c
Erik Kimsey <kimseyerik@gmail.com>
2019-06-01 14:11:16 -0400

    Returning data from API.  Form taking input and setting to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ItemList from './components/ItemList';
+
+const HN_URL = 'https://hacker-news.firebaseio.com/v0/';
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: '',
+			result: ''
+		};
+		this.input = React.createRef();
+	}
+
+	componentDidMount() {
+		axios
+			.get(HN_URL + 'item/8863.json?print=pretty')
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		e.target.reset();
+	};
+
+	handleChange = (e) => {
+		console.log(e.target.value);
+		this.setState({ query: e.target.value });
+	};
+
+	render() {
+		return (
+			<div className="App-header">
+				<ItemList />
+				<div className="search-container">
+					<form onSubmit={this.handleSubmit}>
+						<label>
+							Name:
+							<input
+								type="text"
+								ref={this.input}
+								value={this.state.value}
+								name=""
+								onChange={this.handleChange}
+							/>
+						</label>
+						<input type="submit" value="Submit" />
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
