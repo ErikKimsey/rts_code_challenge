415d3a4bb0c84ec2bf96135cda090d6397b4c11d
Erik Kimsey <kimseyerik@gmail.com>
2019-06-02 14:06:42 -0400

    completed code exercises

--- rts_labs_code_exercises.js
+++ rts_labs_code_exercises.js
@@ -44,6 +44,9 @@ function rotateChars(str, input) {
  * 
  * #3
  * One aspect of my favorite framework/language/platform that I'd like to change?
- * 
+ * I can't say Javascript is my favorite language.  However, given it's the language I use the most, even in personal projects, I'll pick on Javascript.  
+ * Javascript's evolution has been great.  There's so much it can accomplish, these days.  Javascript APIs, in particular, I've been excited to use have been those pertaining to mobile device hardware (e.g., gyroscope, accelerometer, etc).  Another exciting aspect of Javascript APIs has been their use in creating Progressive Web Apps.  
+ * HOWEVER!  Without the use of a JS-to-native bridge such as NativeScript or ReactNative, Javascript cannot take advangtage of background processing.  JS has Service Workers, which allow for events like push notifications while the browser is closed.  But,...
+ * TL;DR:
+ * I would like for JavaScript, via PWA, to be able to run background processes to listen to device events while the browser is closed. 
  */
-
