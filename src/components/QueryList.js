aa79676fa5086224ab393951264e42450bba2dde
Erik Kimsey <kimseyerik@gmail.com>
2019-06-02 08:05:18 -0400

    Styles to query list

--- src/components/QueryList.js
+++ src/components/QueryList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './styles/query_list.scss';
 
 export default function QueryList(props) {
 	return (
-		<div>
+		<div className="query-list-container">
+			<div className="queries-header">Queries Made:</div>
 			{props.list.map((e) => {
-				return <div>{e.text}</div>;
+				return <div className="query">"{e.text}"</div>;
 			})}
 		</div>
 	);
