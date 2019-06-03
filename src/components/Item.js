a83c3295a406cb459e4a4ebecc57d248f88ee547
Erik Kimsey <kimseyerik@gmail.com>
2019-06-03 10:15:04 -0400

    Added and formatted article pub date to item.js. Restyled list item

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 export default function Item(props) {
 	let item = props.data;
+	let date = item.created_at;
+	console.log(date);
 
 	return (
 		<div className="item">
-			<div>{item.title}</div>
+			<div className="title">{item.title}</div>
 			<a href={item.url} target="_blank">
 				{item.url}
 			</a>
+			<div className="date">
+				{item.created_at}
+			</div>
 		</div>
 	);
 }
